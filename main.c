76b645571f4c4e2097b452b742c5d6574af46ca0
Bruno Samuel <brunosag02@gmail.com>
2023-03-14 12:08:08 -0300

    adicionar desenho de sprites

--- main.c
+++ main.c
@@ -3,21 +3,30 @@
 
 int main()
 {
-    // -------------------------------------------------------------------- //
-    // Initialize                                                           //
-    // -------------------------------------------------------------------- //
+    // ---------------------------------------------------------------------------------------- //
+    // Initialize                                                                               //
+    // ---------------------------------------------------------------------------------------- //
     const int screenWidth = 1200;
     const int screenHeight = 800;
     const int levelWidth = 30;
     const int levelHeight = 20;
+    const int elementSize = 40;
 
+    // Incializar janela do jogo
     InitWindow(screenWidth, screenHeight, "TerraINF");
+
+    // Carregar sprites
+    Texture2D backgroundTexture = LoadTexture("sprites/background.png");
+    Texture2D borderTexture = LoadTexture("sprites/border.png");
+    Texture2D dirtTexture = LoadTexture("sprites/dirt.png");
+    Texture2D oreTexture = LoadTexture("sprites/ore.png");
+    Texture2D playerTexture = LoadTexture("sprites/player.png");
 
     while (!WindowShouldClose())
     {
-        // -------------------------------------------------------------------- //
-        // Update                                                               //
-        // -------------------------------------------------------------------- //
+        // ------------------------------------------------------------------------------------ //
+        // Update                                                                               //
+        // ------------------------------------------------------------------------------------ //
 
         // Abrir arquivo texto do nível
         FILE *levelFile = fopen("nivel1.txt", "r");
@@ -43,12 +52,45 @@ int main()
         // Fechar arquivo texto do nível
         fclose(levelFile);
 
-        // -------------------------------------------------------------------- //
-        // Draw                                                                 //
-        // -------------------------------------------------------------------- //
+        // ------------------------------------------------------------------------------------ //
+        // Draw                                                                                 //
+        // ------------------------------------------------------------------------------------ //
         BeginDrawing();
 
-        ClearBackground(RAYWHITE);
+        // Desenhar texturas com base na matriz
+        for (int i = 0; i < levelHeight; i++)
+        {
+            for (int j = 0; j < levelWidth; j++)
+            {
+                // Verificar elemento atual na matriz
+                Texture2D currentTexture;
+                switch (level[i][j])
+                {
+                case 'G':
+                case 'T':
+                case 'S':
+                case 'U':
+                case 'C':
+                    currentTexture = oreTexture;
+                    break;
+                case 'X':
+                    currentTexture = dirtTexture;
+                    break;
+                case ' ':
+                    currentTexture = backgroundTexture;
+                    break;
+                case 'B':
+                    currentTexture = borderTexture;
+                    break;
+                case 'J':
+                    currentTexture = playerTexture;
+                    break;
+                }
+
+                // Desenhar elemento
+                DrawTexture(currentTexture, (j * elementSize), (i * elementSize), WHITE);
+            }
+        }
 
         EndDrawing();
     }
