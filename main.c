e8c9843bd2a4ce3137e8b102101dc73b68fa52d3
Bruno Samuel <brunosag02@gmail.com>
2023-03-17 15:30:34 -0300

    adicionar HUD e variáveis

--- main.c
+++ main.c
@@ -1,5 +1,17 @@
 #include "raylib.h"
 #include <stdio.h>
+#define MAX_NAME 3
+
+typedef struct player
+{
+    Vector2 location;
+    char name[MAX_NAME + 1];
+    int score;
+    int lives;
+    int energy;
+    int ladders;
+    char lastMined;
+} t_player;
 
 int main()
 {
@@ -11,6 +23,7 @@ int main()
     const int levelWidth = 30;
     const int levelHeight = 20;
     const int elementSize = 40;
+    const int HUDFontSize = 26;
 
     // Incializar janela do jogo
     InitWindow(screenWidth, screenHeight, "TerraINF");
@@ -19,8 +32,15 @@ int main()
     Texture2D backgroundTexture = LoadTexture("sprites/background.png");
     Texture2D borderTexture = LoadTexture("sprites/border.png");
     Texture2D dirtTexture = LoadTexture("sprites/dirt.png");
+    Texture2D HUDTexture = LoadTexture("sprites/hud.png");
     Texture2D oreTexture = LoadTexture("sprites/ore.png");
     Texture2D playerTexture = LoadTexture("sprites/player.png");
+
+    // Inicalizar jogador
+    t_player player = {{2, 11}, {0}, 0, 3, 100, 20, 0};
+
+    // Definir nível inicial
+    int currentLevel = 1;
 
     while (!WindowShouldClose())
     {
@@ -91,6 +111,16 @@ int main()
                 DrawTexture(currentTexture, (j * elementSize), (i * elementSize), WHITE);
             }
         }
+
+        // Desenhar HUD
+        DrawTexture(HUDTexture, 0, 0, WHITE);
+        DrawText(TextFormat("%i", player.lives), 66, 8, HUDFontSize, RAYWHITE);
+        DrawText(TextFormat("%i", player.energy), 177, 8, HUDFontSize, RAYWHITE);
+        DrawText(TextFormat("%i", player.ladders), 311, 8, HUDFontSize, RAYWHITE);
+        DrawText(TextFormat("%i", player.score),
+                 (956 - MeasureText(TextFormat("%i", player.score), 28)), 8, 28, RAYWHITE);
+        DrawText("/1000", 962, 14, 20, DARKGRAY);
+        DrawText(TextFormat("Nível %i", currentLevel), 1080, 8, HUDFontSize, RAYWHITE);
 
         EndDrawing();
     }
